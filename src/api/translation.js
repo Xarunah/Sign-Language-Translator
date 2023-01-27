533e15aeaf7b2e98889503e0dcd54cd222817973
JonasAmiri <jamiri17@student.aau.dk>
2023-01-27 22:05:07 +0100

    logout and clear history

--- src/api/translation.js
+++ src/api/translation.js
@@ -23,3 +23,23 @@ export const storeUserTranslation=async(translation, user)=> {
     }
 }
 
+export const clearHistory = async (user) => {
+    try {
+        const response = await fetch(`${apiUrl}/${user.id}`, {
+            method: "PATCH",
+            headers: createHeaders(),
+            body: JSON.stringify({
+                username: user.username,
+                translations: []
+            })
+        });
+        if (!response.ok) {
+            throw new Error('Could not update translations history')
+        }
+        const result = await response.json()
+        return [null, result]
+    } catch (err) {
+        return [err.message, null]
+    }
+}
+
--- src/components/Profile/ProfileTranslationHistory.jsx
+++ src/components/Profile/ProfileTranslationHistory.jsx
@@ -1,9 +1,33 @@
+import { clearHistory } from "../../api/translation";
+import { STORAGE_KEY_USER } from "../../const/storageKeys";
+import { useUser } from "../../context/UserContext";
 import ProfileTranslationHistoryItem from "./ProfileTranslationHistoryItem"
 
 const ProfileTranslationHistory= ({translations}) => {
-
+    const { user, setUser } = useUser()
     const translationList = translations.slice(-10).reverse().map((translation,index) => <ProfileTranslationHistoryItem key={index} translationItem={translation}/>)
 
+    function handleLogout() {
+        localStorage.clear();
+        window.location.reload();
+    }
+
+    async function handleClearHistory() {
+        try {
+            await clearHistory(user);
+            // setUser({
+            //     ...user,
+            //     translations: []
+            // });
+            // localStorage.setItem(STORAGE_KEY_USER, JSON.stringify({
+            //     ...user,
+            //     translations: []
+            // }))
+            // window.location.reload();
+        } catch(e) {
+            alert("Oops, an error occurred!");
+        }
+    }
 
     return (
 
@@ -15,6 +39,8 @@ const ProfileTranslationHistory= ({translations}) => {
                     {translationList}
 
                 </ul>
+                <button onClick={handleClearHistory}>Clear History</button>
+                <button onClick={handleLogout}>Logout</button>
             </section>
         </div>
     )
