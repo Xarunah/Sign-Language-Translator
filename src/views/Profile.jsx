9fc2a749c8a4e0af2807fc3ca4a255cc038dfe4b
JonasAmiri <jamiri17@student.aau.dk>
2023-01-27 11:44:50 +0100

    rebase
    transaltionApiOverWriteIsFixed

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,20 +1,33 @@
+import { findUserById } from "../api/user"
 import ProfileActions from "../components/Profile/ProfileActions"
 import ProfileHeader from "../components/Profile/ProfileHeader"
 import ProfileTranslationHistory from "../components/Profile/ProfileTranslationHistory"
 import { useUser } from "../context/UserContext"
 import withAuth from "../hoc/withAuth"
+import useEffect from "react"
 
-const Profile = () => {
+function Profile() {
 
-    const {user} = useUser()
+    const { user, setUser } = useUser()
+    useEffect(() => {
+        const findUser = async () => {
+            const [error, latestUser] = await findUserById(user.id)
+            if (error !== null) {
+                setUser(latestUser)
+            }
+        }
 
-    return(
+        findUser()
+
+    }, [setUser, user.id])
+
+    return (
         <><h1>Profile</h1>
-        <ProfileHeader username={user.username}/>
-        <ProfileActions/>
-        <ProfileTranslationHistory translations={user.translations}/>
+            <ProfileHeader username={user.username} />
+            <ProfileActions />
+            <ProfileTranslationHistory translations={user.translations} />
         </>
-        
+
     )
 }
 export default withAuth(Profile)
